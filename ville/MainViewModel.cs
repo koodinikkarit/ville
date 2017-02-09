c68d1d8f57c893092deca921ddbf12bd866cd0b3
Jaska <pusanteemu@gmail.com>
2017-02-09 07:56:46 +0200

    Muokattu käyttöliittymää sekä luotu MainViewModel.

---
+++ ville/MainViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
+
+namespace ville
+{
+    class MainViewModel : INotifyPropertyChanged
+    {    
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            //comPorts = new ObservableCollection<string>(SerialPort.GetPortNames());
+        }
+
+        private string selectedComPort;
+
+        public string Selectedcomport
+        {
+            get { return selectedComPort; }
+            set { selectedComPort = value; }
+        }
+
+        private byte macPartOne;
+
+        public byte Macpartone
+        {
+            get { return macPartOne; }
+            set { macPartOne = value; }
+        }
+
+
+        private byte macPartTwo;
+
+        public byte Macparttwo
+        {
+            get { return macPartTwo; }
+            set { macPartTwo = value; }
+        }
+
+
+        private byte macPartThree;
+
+        public byte Macpartthree
+        {
+            get { return macPartThree; }
+            set { macPartThree = value; }
+        }
+
+        private byte macPartFour;
+
+        public byte Macpartfour
+        {
+            get { return macPartFour; }
+            set { macPartFour = value; }
+        }
+
+        private byte macPartFive;
+
+        public byte Macpartfive
+        {
+            get { return macPartFive; }
+            set { macPartFive = value; }
+        }
+
+        private byte macPartSix;
+
+        public byte Macpartsix
+        {
+            get { return macPartSix; }
+            set { macPartSix = value; }
+        }
+
+
+        private string petriIp;
+
+        public string Petriip
+        {
+            get { return petriIp; }
+            set { petriIp = value; }
+        }
+
+
+        private int petriPort;
+
+        public int Petriport
+        {
+            get { return petriPort; }
+            set { petriPort = value; }
+        }
+
+
+        private ObservableCollection<string> comPorts;
+
+        public ObservableCollection<string> Comports
+        {
+            get { return comPorts; }
+            set { comPorts = value; }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
--- ville/MainWindow.xaml.cs
+++ ville/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace ville
 {
@@ -20,9 +21,57 @@ namespace ville
     /// </summary>
     public partial class MainWindow : Window
     {
+        //MainViewModel mainViewModel = new MainViewModel();
         public MainWindow()
         {
             InitializeComponent();
+            Console.WriteLine(SerialPort.GetPortNames()[0]);
+            //this.DataContext = mainViewModel;
+        }
+
+        private void macPartOne_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void macPartThree_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void macPartTwo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void macPartFive_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void macPartSix_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void macPartFour_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void petriIpTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void petriPortTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void saveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
